fc3f472813973e4971c7139638d60ad703329807
ryan-montoya <rmonto6@uw.edu>
2023-03-23 14:19:07 -0700

    learning traits and CLI best practices.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+
+struct Parameters { 
+    query_trait : String,  
 }
+//use std::env;
+//use std::io::stdin; 
+/* 
+trait ReadParam { 
+   fn read(&self);
+}
+
+impl ReadParam for Parameters  { 
+    fn read(&self)  { 
+    
+    }
+}
+*/
+fn main() {
+    println!("Hello, world!");  
+   
+    let temp : String = read(); 
+    let query = Parameters { query_trait: temp};
+} 
+
+
+fn read() -> String {   
+    let mut line = String::new();
+    println!("Enter query trait:");
+    let _b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Hello , {}", line); 
+    return line; 
+
+   // println!("no of bytes read , {}", b1);
+} 
+
