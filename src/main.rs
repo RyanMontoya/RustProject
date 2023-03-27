2410c266fee651aaa504a946f77b63a62365dc12
ryan-montoya <rmonto6@uw.edu>
2023-03-27 16:28:43 -0700

    learning rust data formats to best suite traits.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+
 struct Parameters { 
     query_trait : String,  
 }
@@ -14,15 +15,30 @@ impl ReadParam for Parameters  {
     
     }
 }
-*/
-fn main() {
+*/ 
+
+enum TraitVariants { 
+    TraitA(String), 
+    TraitB(String, String, String),
+}
+
+fn main() { 
+ 
+
+
     println!("Hello, world!");  
    
     let temp : String = read(); 
     let query = Parameters { query_trait: temp}; 
-    println!("howdy, {}", query.query_trait);
-} 
+    println!("howdy, {}", query.query_trait); 
+    boxlearn(); 
+    let a = TraitVariants::TraitA(String::from("concentration"));   
+    let b = TraitVariants::TraitB(String::from("concentration"),String::from("move"),String::from("beans"));
+    //let b = TraitVariants::TraitB("concentration","move",);
+    
+   // println!("multi trait enum pass {}",b);
 
+} 
 
 fn read() -> String {   
     let mut line = String::new();
@@ -32,5 +48,17 @@ fn read() -> String {
     return line; 
 
    // println!("no of bytes read , {}", b1);
+}  
+
+
+fn boxlearn() { 
+    let boxtrait = Box::new(10); 
+    println!("box containing {}", boxtrait); 
+
 } 
+
+fn enumlearn() { 
+
+}
+
 
